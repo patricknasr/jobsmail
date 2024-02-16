047b814b8073af24e88bf336fd2562cde4ce45f5
Patrick Nasr <nasr7601@hotmail.com>
2024-02-16 19:13:13 +1100

    remove comments

--- src/send_email.py
+++ src/send_email.py
@@ -8,10 +8,8 @@ import os
 def send_email(to_email):
     
     try: 
-        #Create your SMTP session 
         smtp = smtplib.SMTP('smtp.gmail.com', 587) 
-
-    #Use TLS to add security 
+ 
         smtp.starttls() 
 
         load_dotenv()
@@ -29,14 +27,11 @@ def send_email(to_email):
         msg['To'] = "pnasrwork@gmail.com"
         msg['Subject'] = "Test Email"
 
-        # Email body
         body = f"Hello {name}, greeting from {date_time}."
         msg.attach(MIMEText(body, 'plain'))
 
-        #Sending the Email
         smtp.sendmail("jobsmailaus@gmail.com", "pnasrwork@gmail.com",msg.as_string()) 
 
-        #Terminating the session 
         smtp.quit() 
         print ("Email sent successfully!") 
 
