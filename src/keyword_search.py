acb541d0ea4945076d8e8cc579eaa0273e9b3165
Patrick Nasr <nasr7601@hotmail.com>
2024-02-16 19:31:08 +1100

    update searching

--- src/keyword_search.py
+++ src/keyword_search.py
@@ -2,11 +2,19 @@ import csv
 import datetime
 
 def search_and_store(results_file, url, soup, selector, keywords):
-    elements = soup.select(selector)
-    for element in elements:
-        if any(keyword.lower() in element.text.lower() for keyword in keywords):
-            with open(results_file, mode='a', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow([datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), url])
-            return True
-    return False
+    elements = soup.select('#content > div > form > div.js-openings-list.opening-list.col-md-9 > div')
+
+    found = False  
+
+    graduate_children = [
+        child.text.replace('\n', '').replace('  ', '') for child in elements
+        if "Graduate" in child.text
+    ]  
+
+    with open(results_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        # Now storing element_text which has no newlines
+        writer.writerow([datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), url, graduate_children])
+        found = True
+
+    return found
